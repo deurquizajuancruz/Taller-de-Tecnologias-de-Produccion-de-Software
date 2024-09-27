fa714f39319cea10e7fbca80971ada08cae0bd14
Juan Cruz de Urquiza <deurquizajuancruz@gmail.com>
2024-09-27 17:40:08 -0300

    update datastructure

--- Problemas sobre estructuras de datos/datastructure.py
+++ Problemas sobre estructuras de datos/datastructure.py
@@ -2,22 +2,6 @@ from collections import deque
 from sys import stdin
 
 
-class PriorityQueue(object):
-    def __init__(self):
-        self.queue = []
-
-    def insert(self, data):
-        self.queue.append(data)
-
-    def delete(self, element):
-        try:
-            max_val = max(self.queue)
-            self.queue.remove(max_val)
-            return max_val == element
-        except:
-            return False
-
-
 def delete_stack(stack, element):
     try:
         return stack.pop() == element
@@ -32,10 +16,19 @@ def delete_queue(queue, element):
         return False
 
 
+def delete_priority(priority, element):
+    try:
+        max_val = max(priority)
+        priority.remove(max_val)
+        return max_val == element
+    except:
+        return False
+
+
 def load_structures(number_operations):
     stack = deque()  # creo una pila
     queue = deque()  # creo una cola
-    priority_queue = PriorityQueue()  # creo una cola con prioridad
+    priority_queue = []  # creo una cola con prioridad
     can_be_stack = True
     can_be_queue = True
     can_be_priority_queue = True
@@ -45,14 +38,14 @@ def load_structures(number_operations):
         if int(operation[0]) == 1:
             stack.append(element)
             queue.append(element)
-            priority_queue.insert(element)
+            priority_queue.append(element)
         else:
             if can_be_stack:
                 can_be_stack = delete_stack(stack, element)
             if can_be_queue:
                 can_be_queue = delete_queue(queue, element)
             if can_be_priority_queue:
-                can_be_priority_queue = priority_queue.delete(element)
+                can_be_priority_queue = delete_priority(priority_queue, element)
     if can_be_stack and not can_be_queue and not can_be_priority_queue:
         return "stack"
     if can_be_queue and not can_be_stack and not can_be_priority_queue:
