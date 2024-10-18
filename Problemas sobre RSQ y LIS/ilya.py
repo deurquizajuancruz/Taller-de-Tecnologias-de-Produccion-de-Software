cca9c0cc1005042f80cdf19b5d8cfd32730b0dcd
Juan Cruz de Urquiza <deurquizajuancruz@gmail.com>
2024-10-18 15:27:18 -0300

    improve lis

--- Problemas sobre RSQ y LIS/ilya.py
+++ Problemas sobre RSQ y LIS/ilya.py
@@ -1,8 +1,17 @@
 from sys import stdin
 
 
-def fill_array(sequence):
-    sequence = list(sequence)
+def fill_array(sequence: list[int]) -> list[int]:
+    """
+    Llena un array con la cantidad de coincidencias de la secuencia por posición;
+    utilizando programación dinámica
+    Params:
+        sequence: list[int]
+            secuencia de string leídas
+    Returns:
+        equals: list[int]
+            array con la cantidad de coincidencias de la secuencia leída por posición
+    """
     n = len(sequence)
     equals = [0] * n
     for i in range(n - 2, -1, -1):
@@ -14,7 +23,7 @@ def fill_array(sequence):
 
 sequence = stdin.readline().strip()  # Se lee el string
 queries = int(stdin.readline().strip())  # Se lee la cantidad de queries
-result_array = fill_array(sequence)
-for i in range(queries):
+result_array = fill_array(list(sequence))
+for _ in range(queries):
     indexes = stdin.readline().strip().split(" ")
     print(result_array[int(indexes[0]) - 1] - result_array[int(indexes[1]) - 1])
